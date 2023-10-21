884bb82d94eea1dcaf771ad0353b37786a66b20f
ANKIT <kumarshrivastav1024@gmail.com>
2023-10-21 23:16:29 +0530

    created signin api route

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js"
 import bcryptjs from "bcryptjs"
+import jwt from "jsonwebtoken"
 import ErrorHandler from "../utils/error.handler.js"
 class AuthController{
     async signup(req,res,next){
@@ -20,6 +21,26 @@ class AuthController{
             return res.status(201).send({message:"User Registered Successfully"})
         } catch (error) {
            next(ErrorHandler(501,error.message))
+        }
+    }
+    async signin(req,res,next){
+        try {
+            const {email,password}=req.body
+            const user=await User.findOne({email})
+            if(!user){
+                return next(ErrorHandler(404,"User Not Found!"))
+            }
+            const isValidPwd=await bcryptjs.compare(password,user.password)
+            if(!isValidPwd){
+                return next(ErrorHandler(401,"Wrong Credentials"))
+            }
+            const accessToken=jwt.sign({id:user._id},process.env.JWT_ACCESS_TOKEN_SECRET_KEY,{expiresIn:1000*60*60})
+            res.cookie('accessToken',accessToken,{httpOnly:true,maxAge:1000*60*60})
+            return res.send({message:"User Login Successfully"})
+
+
+        } catch (error) {
+            next(error)
         }
     }
 }
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -2,4 +2,5 @@ import express from "express"
 import authController from "../controller/auth.controller.js"
 const router=express.Router()
 router.post("/signup",authController.signup)
+router.post("/signin",authController.signin)
 export default router
