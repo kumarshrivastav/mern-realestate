83497162f3ace652a416abe622a1d9ba680aa2d6
ANKIT <kumarshrivastav1024@gmail.com>
2023-11-01 23:19:58 +0530

    pre installation

--- api/index.js
+++ api/index.js
@@ -4,16 +4,22 @@ import userRouter from "../api/routes/user.route.js"
 import listingRouter from "../api/routes/listing.route.js"
 import authRouter from "../api/routes/auth.route.js"
 import ConnectDB from "./DbConfig.js"
+import path from "path"
 import cookieParser from "cookie-parser"
 dotenv.config()
 const app=express()
 ConnectDB()
+const __dirname=path.resolve()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 app.use("/api/user/",userRouter)
 app.use("/api/auth/",authRouter)
 app.use("/api/listing/",listingRouter)
+app.use(express.static(path.join(__dirname,'/client/dist')))
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'client','dist','index.html'))
+})
 const server=app.listen(3000,()=>{
     console.log(`Server started at http://localhost:${server.address().port}`)
 })
